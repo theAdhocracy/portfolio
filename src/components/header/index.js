ec9c47b1ad8f4e768fa7daec53fc231e0ed3223e
theAdhocracy <murrayadcock@gmail.com>
2020-03-29 18:30:26 +0100

    :lipstick: Landing page styles and structure

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,10 +4,22 @@ import styles from "./header.module.css"
 // Components
 import Nav from "../nav"
 
-const Header = () => {
+// Assets
+
+const Header = ({ logomarkColour }) => {
 	return (
 		<header className={styles.header}>
-			<h1>Murray Adcock</h1>
+			<svg
+				style={{ "--logomark-colour": `${logomarkColour}` }}
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 216.35 216.07"
+			>
+				<polygon points="108.35 0 0.35 216 72.35 216 108.35 153 144.35 216 216.35 216 108.35 0" />
+				<polygon points="95.35 216.07 121.36 216.07 108.35 193.31 95.35 216.07" />
+				<polygon points="57.31 57.37 0 0.07 0 171.98 57.31 57.37" />
+				<polygon points="144.26 71.81 108 108.07 72.21 72.28 0.35 216 72.35 216 108.35 153 144.35 216 216 216 216 215.29 144.26 71.81" />
+				<polygon points="216 170.57 216 0.07 159.16 56.9 216 170.57" />
+			</svg>
 			<Nav />
 		</header>
 	)
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,17 +4,23 @@ import { Link } from "gatsby"
 // Components
 import Header from "../Components/Header"
 
+// Assets
+import profilePhoto from "../assets/murray-profile-square.jpg"
+
 export default () => (
 	<>
-		<Header />
-		<main>
-			<h2>Hi, I'm Murray 👋</h2>
-			<p>
-				I'm a front-end developer, copywriter, and content creator living in
-				South West London.
-			</p>
-			<p>In other words, I make things. Want to see what I've made?</p>
-			<Link>Explore my portfolio</Link>
+		<Header logomarkColour="#bcd9ea" />
+		<main className="landing">
+			<img src={profilePhoto} alt="Murray Adcock" />
+			<section className="about-me">
+				<h2>Hi, I'm Murray 👋</h2>
+				<p>
+					I'm a front-end developer, copywriter, and content creator living
+					in South West London.
+				</p>
+				<p>In other words, I make things. Want to see what I've made?</p>
+			</section>
+			<Link className="explore">Explore my portfolio</Link>
 		</main>
 		<footer></footer>
 	</>
