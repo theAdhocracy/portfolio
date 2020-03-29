4480c0ee63c2c85e42f3cc4d389d0e1114297736
theAdhocracy <murrayadcock@gmail.com>
2020-03-29 13:36:42 +0100

    :sparkles: Setting up global stylesheets and gatsby-browser

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,27 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default () => (
-  <div style={{ color: `purple` }}>
-    <h1>Hello Gatsby!</h1>
-    <p>What a world.</p>
-  </div>
+	<>
+		<header>
+			<h1>Murray Adcock</h1>
+			<nav>
+				<ul>
+					<li>Coding</li>
+					<li>Creative</li>
+					<li>Contact</li>
+				</ul>
+			</nav>
+		</header>
+		<main>
+			<h2>Hi, I'm Murray 👋</h2>
+			<p>
+				I'm a front-end developer, copywriter, and content creator living in
+				South West London.
+			</p>
+			<p>In other words, I make things. Want to see what I've made?</p>
+			<Link>Explore my portfolio</Link>
+		</main>
+		<footer></footer>
+	</>
 )
