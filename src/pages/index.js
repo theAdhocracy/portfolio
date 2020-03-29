decbb44383df86ca6bcf94d5ed2205da516a3768
theAdhocracy <murrayadcock@gmail.com>
2020-03-29 20:32:51 +0100

    :sparkles: Initial layout and mobile styles for highlights section

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,17 +11,53 @@ const IndexPage = () => {
 	return (
 		<>
 			<Header accentColour="#bcd9ea" />
-			<main className="landing">
-				<img src={profilePhoto} alt="Murray Adcock" />
-				<section className="about-me">
-					<h2>Hi, I'm Murray 👋</h2>
-					<p>
-						I'm a <em>front-end developer</em>, <em>copywriter</em>, and{" "}
-						<em>content creator</em> living in South West London.
-					</p>
-					<p>In other words, I make things. Want to see what I've made?</p>
+			<main>
+				<div className="landing">
+					<img src={profilePhoto} alt="Murray Adcock" />
+					<section className="about-me">
+						<h2>Hi, I'm Murray 👋</h2>
+						<p>
+							I'm a <em>front-end developer</em>, <em>copywriter</em>,
+							and <em>content creator</em> living in South West London.
+						</p>
+						<p>
+							In other words, I make things. Want to see what I've made?
+						</p>
+					</section>
+				</div>
+				<Link className="explore" to="#latest-coding">
+					Explore my portfolio
+				</Link>
+				<section id="latest-coding" className="highlight">
+					<h2>Coding Highlight</h2>
+					<img src={profilePhoto} />
+					<div className="details">
+						<h3>Director Interview Bloopers</h3>
+						<p>Talent Point</p>
+						<p>
+							Lorem ipsum dolor sit amet. Non squiret e pluribus unum et
+							del latro, sunc deus amo casseus. Lorem ipsum dolor sit
+							amet. Non squiret e pluribus unum et del latro, sunc deus
+							amo casseus.
+						</p>
+						<Link>Learn more</Link>
+					</div>
 				</section>
-				<Link className="explore">Explore my portfolio</Link>
+				<section id="latest-creative" className="highlight">
+					<h2>Creative Highlight</h2>
+					<img src={profilePhoto} />
+					<div className="details">
+						<h3>Director Interview Bloopers</h3>
+						<p>Talent Point</p>
+						<p>
+							Lorem ipsum dolor sit amet. Non squiret e pluribus unum et
+							del latro, sunc deus amo casseus. Lorem ipsum dolor sit
+							amet. Non squiret e pluribus unum et del latro, sunc deus
+							amo casseus.
+						</p>
+						<Link>Learn more</Link>
+					</div>
+				</section>
 			</main>
 			<footer></footer>
 		</>
