a8d871aa67be56ea3164e89f0b3b15a432319e77
theAdhocracy <murrayadcock@gmail.com>
2020-03-29 19:18:06 +0100

    :sparkles: Enabling per page accent theming

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,21 +7,25 @@ import Header from "../Components/Header"
 // Assets
 import profilePhoto from "../assets/murray-profile-square.jpg"
 
-export default () => (
-	<>
-		<Header logomarkColour="#bcd9ea" />
-		<main className="landing">
-			<img src={profilePhoto} alt="Murray Adcock" />
-			<section className="about-me">
-				<h2>Hi, I'm Murray 👋</h2>
-				<p>
-					I'm a front-end developer, copywriter, and content creator living
-					in South West London.
-				</p>
-				<p>In other words, I make things. Want to see what I've made?</p>
-			</section>
-			<Link className="explore">Explore my portfolio</Link>
-		</main>
-		<footer></footer>
-	</>
-)
+const IndexPage = () => {
+	return (
+		<>
+			<Header accentColour="#bcd9ea" />
+			<main className="landing">
+				<img src={profilePhoto} alt="Murray Adcock" />
+				<section className="about-me">
+					<h2>Hi, I'm Murray 👋</h2>
+					<p>
+						I'm a <em>front-end developer</em>, <em>copywriter</em>, and{" "}
+						<em>content creator</em> living in South West London.
+					</p>
+					<p>In other words, I make things. Want to see what I've made?</p>
+				</section>
+				<Link className="explore">Explore my portfolio</Link>
+			</main>
+			<footer></footer>
+		</>
+	)
+}
+
+export default IndexPage
